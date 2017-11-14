638643e240547756c834c3becfff426b174ccad4
Heyder Dias <heyderpd@gmail.com>
2017-11-13 22:46:12 -0200

    first code

---
+++ src/index.js
@@ -0,0 +1,82 @@
+import Peer from 'peerjs'
+import randomstring from 'randomstring'
+
+const peerConnection = key => {
+  const state = {
+    peer: null,
+    connection: null,
+    onData: null,
+    opened: false,
+    host: false,
+    join: false
+  }
+
+  const _onOpen = () => {
+    state.opened = true
+  }
+
+  const _onClose = () => {
+    state.opened = false
+  }
+
+  const _setHost = () => {
+    state.host = true
+    state.join = false
+  }
+
+  const _setJoin = () => {
+    state.host = false
+    state.join = true
+  }
+
+  const _initialize = key => {
+    state.peer = new Peer(randomstring.generate(5), { key })
+  }
+
+  const _safeOnData = data => {
+    if (typeof(state.onData) === 'function') {
+      state.onData(data)
+    }
+  }
+
+  const _onConnection = conn => {
+    state.connection = conn
+    state.peer.on('open', _onOpen)
+    state.peer.on('close', _onClose)
+    state.peer.on('error', err => (_onClose(), console.log('-*-error', err)))
+    conn.on('data', _safeOnData)
+  }
+
+  const object = {}
+
+  object.host = () => {
+    _setHost()
+    state.peer.on('connection', _onConnection)
+    return object
+  }
+
+  object.join = id => {
+    _setJoin()
+    const conn = state.peer.connect(id)
+    _onConnection(conn)
+    return object
+  }
+
+  object.getId = () => state.peer.id
+
+  object.setOnData = onData => {
+    state.onData = onData
+    return object
+  }
+
+  object.send = data => {
+    state.connection.send(data)
+    return object
+  }
+
+  _initialize(key)
+
+  return object
+}
+
+export default peerConnection
